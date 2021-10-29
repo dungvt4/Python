ca103a0daad5853d01cd7355a4b3800b0f83cbf4
Dung Vũ <dungvt4@onemount.com>
2021-10-29 23:12:16 +0700

    uppdate

--- python-hw4-0907/hw-fnc-countCharType.py
+++ python-hw4-0907/hw-fnc-countCharType.py
@@ -3,17 +3,16 @@
 
 
 s = input("Mời bạn nhập chuỗi kí tự:")
-print(type(s))
 rslt_dict = {"LETTERS":0, "CASE": {"UPPER CASE":0, "LOWER CASE":0}, "DIGITS":0}
 dict_case = {"UPPER CASE":0, "LOWER CASE":0}
 
-#=========================Hàm dùng bất kì built-in nào=========================
+#=========================Hàm chỉ dùng 1 hàm built-in=========================
 def count_char_type(s):
     upper_case = 0
     lower_case = 0
     digit = 0
-    for i in range(len(s)):
-        s_int = ord(s[i])
+    for char in s:
+        s_int = ord(char)
         if s_int in range(65, 91):
             upper_case += 1
         elif s_int in range(97, 123):
@@ -28,22 +27,23 @@ def count_char_type(s):
     rslt_dict["CASE"] = dict_case
     return rslt_dict
 
-count_char_type(s)
-rslt_dict
+print(count_char_type(s))
 
 
-#=========================Hàm chỉ dùng 1 hàm built-in=========================
+
+#=========================Hàm dùng bất kì built-in nào=========================
 def count_char_type2(s):
     upper_case = 0
     lower_case = 0
     digit = 0
+    ASCII_char = "".join(chr(x) for x in range(97, 123))
+    ASCII_digit = "".join(chr(x) for x in range(48, 58))
     for x in s:
-        s_int = ord(x)
-        if s_int in range(65, 91):
+        if x in ASCII_char.upper():
             upper_case += 1
-        elif s_int in range(97, 123):
+        elif x in ASCII_char:
             lower_case += 1
-        elif s_int in range(48, 58):
+        elif x in ASCII_digit:
             digit += 1
     letter = upper_case + lower_case
     rslt_dict["LETTERS"] = letter
@@ -53,5 +53,4 @@ def count_char_type2(s):
     rslt_dict["CASE"] = dict_case
     return rslt_dict
 
-count_char_type2(s)
-rslt_dict
+print(count_char_type2(s))
