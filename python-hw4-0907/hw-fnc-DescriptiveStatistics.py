58d0feef706fa02fb78e5205b0fd29687bf0e4d0
Dung Vũ <dungvt4@onemount.com>
2021-10-29 23:59:34 +0700

    update

--- python-hw4-0907/hw-fnc-DescriptiveStatistics.py
+++ python-hw4-0907/hw-fnc-DescriptiveStatistics.py
@@ -2,8 +2,8 @@ from collections import Counter
 
 input_lst = input("Mời bạn nhập dãy số điểm thi:").split()
 sorted_lst = []
-for i in range(len(input_lst)):
-    sorted_lst.append(int(input_lst[i]))
+for point in input_lst:
+    sorted_lst.append(int(point))
 sorted_lst = sorted(sorted_lst)
 print(sorted_lst)
 lst_mode = []
@@ -23,15 +23,21 @@ def find_median(lst):
 
 def find_mode(lst):
     count = Counter(lst)
-    all_value = count.values()
-    max_value = max (all_value)
-    print(max_value)
-    
-    while len (count):
-        value_tup_gen = count.popitem()
-        my_set_gen.add(value_tup_gen[1])
-        list_dict = list_dict
+    lst_count = count.most_common()
+    lst_mode = [(lst_count[0])[0]]
+    for i in range (len(lst_count)-1):
+        tup = lst_count[i]
+        next_tup = lst_count[i+1]
+        if tup[1] > next_tup[1]:
+            break
+        else: lst_mode.append(next_tup[0])
+    return lst_mode
+            
 
-find_mode(sorted_lst)
+mean = find_mean(sorted_lst)
+median = find_median(sorted_lst)
+mode = find_mode(sorted_lst)
+
+print(mean,median,mode)
 
 
